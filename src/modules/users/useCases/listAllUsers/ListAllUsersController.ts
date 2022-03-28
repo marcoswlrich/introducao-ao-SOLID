b3e36c9325bb24e12cd07f2b79f9f41c332f242e
marcoswlrich <marcosapw.dev@gmail.com>
2022-03-27 22:39:53 -0300

    Users List

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,16 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.headers;
+
+    try {
+      const usersList = this.listAllUsersUseCase.execute({
+        user_id: String(user_id),
+      });
+      return response.status(202).json(usersList);
+    } catch (error) {
+      return response.status(400).json({ error: error.message });
+    }
   }
 }
 
