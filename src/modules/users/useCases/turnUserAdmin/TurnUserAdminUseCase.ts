9fab0ac9a41bac3015056e1d584ef468f1e6096b
marcoswlrich <marcosapw.dev@gmail.com>
2022-03-27 22:13:03 -0300

    Turn User

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
@@ -9,7 +9,15 @@ class TurnUserAdminUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User {
-    // Complete aqui
+    const userId = this.usersRepository.findById(user_id);
+
+    if (!userId) {
+      throw new Error("User not found");
+    }
+
+    userId.admin = true;
+
+    return userId;
   }
 }
 
